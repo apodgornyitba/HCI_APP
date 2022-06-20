a5d883099be40881617893f892da318c258999e4
sanselmo <soanselmo@itba.edu.ar>
2022-06-20 11:24:42 -0300

    layouts y activities devices

---
+++ app/src/main/java/ar/edu/itba/hci_app/DeviceFridge.kt
@@ -0,0 +1,11 @@
+package ar.edu.itba.hci_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeviceFridge : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_device_fridge)
+    }
+}
